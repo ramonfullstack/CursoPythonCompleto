8e452eefdfb105f76da269313b13444ae9b39532
Ramon Silva <ramonss.bque@gmail.com>
2022-09-02 14:31:34 -0300

    Python input

---
+++ python-input.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+nome = input("Digite o seu nome.")
+idade = input("Digite a sua idade")
+print("O seu nome: " + nome + " a sua idade: " + idade)
+
+
+# In[5]:
+
+
+nota1 = input("Digite a primeira nota")
+nota2 = input("Digite a segunda nota")
+nota3 = input("Digite a terceira nota")
+media = int(nota1) + int(nota2) + int(nota3) / 3
+print("A media das notas e: " + str(media))
+
+
+# In[ ]:
+
+
+
+
