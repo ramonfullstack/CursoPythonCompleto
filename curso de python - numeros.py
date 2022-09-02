11fd9faf24dd8ac0ff90478cb6a53acac0708bb9
Ramon Silva <ramonss.bque@gmail.com>
2022-09-02 12:52:49 -0300

    Adicionado arquivo de números python

---
+++ curso de python - numeros.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+numero = 10
+print(numero)
+
+
+# In[3]:
+
+
+numero2 = 2.12
+print(numero2)
+
+
+# In[4]:
+
+
+numero3 = -3
+print(-3)
+
+
+# In[5]:
+
+
+print(numero - numero2)
+
+
+# In[6]:
+
+
+operacao = numero + numero2 + numero3
+
+
+# In[7]:
+
+
+print(operacao)
+
+
+# In[8]:
+
+
+operacao = (numero - numero2) + numero3
+print(operacao)
+
+
+# In[ ]:
+
+
+
+
