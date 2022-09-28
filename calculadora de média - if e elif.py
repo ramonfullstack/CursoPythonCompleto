c050d0643530d45696a64b3b01c56fb0e11bf6d0
Ramon Silva <ramonss.bque@gmail.com>
2022-09-28 20:12:30 -0300

    Add files via upload

---
+++ calculadora de média - if e elif.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+nota1 = 5
+nota2 = 6
+nota3 = 7
+nota4 = 10
+media = (nota1 + nota2 + nota3 + nota4) / 4
+if media == 7 :
+    print("Esta na média")
+elif media < 7:
+    print("Reprovado")
+else :
+    print("Aprovado")
+
+
+# In[ ]:
+
+
+
+
