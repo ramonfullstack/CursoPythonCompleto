8529afe4c1a274ecfd9ebd508fedb95af0fda5f3
Ramon Silva <ramonss.bque@gmail.com>
2022-09-30 23:00:09 -0300

    Add files via upload

---
+++ lacos_de_repeticao_python.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+lista = ["Ramon", "João", "Fernando", "Augusta"]
+for nome in lista:
+    print(nome)
+
+
+# In[3]:
+
+
+frutas = ["Mamão", "Banana", "Maçã"]
+for fruta in frutas:
+    print(fruta)
+
+
+# In[5]:
+
+
+contador = 0
+while contador <= 3: 
+    print(lista[contador])
+    contador += 1
+
+
+# In[6]:
+
+
+contador = 0
+while contador <= 2: 
+    print(frutas[contador])
+    contador += 1
+
+
+# In[ ]:
+
+
+
+
